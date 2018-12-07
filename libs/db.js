3cecff2b444b6fa5994e1af7c089aefe9d25e514
XiaofengQi <qixiaofeng@gsegment.com>
2018-12-07 14:53:38 +0800

    Server ready.

--- libs/db.js
+++ libs/db.js
@@ -1,99 +1,104 @@
 /** Database Infrastructure. (Based on mongodb) */
-
 const { MongoClient:db, ObjectID:OID } = require('mongodb')
 
-let client_instance = false
-let db_instance = false
-let is_preparing = false
+let connect_promise = false
 
 const url = 'mongodb://localhost:27017'
-const db_name = 'be-album'
-const cached_callbacks = []
-
-const exec_callbacks = (cb) => {
-  const temp = cached_callbacks.splice(0, cached_callbacks.length)
-  for (let item of temp) {
-    item()
-  }
-  cb && cb()
-}
 
 const connect = cb => {
-  if (is_preparing) {
-    cached_callbacks.push(cb)
-    return
+  if (connect_promise) {
+    return connect_promise
   }
-  if (client_instance) {
-    exec_callbacks(cb)
-    return
-  }
-  is_preparing = true
-  const ts = Date.now()
-  db.connect(url, { useNewUrlParser: true }).then(client => {
-    is_preparing = false
-    console.log(`Connect cost ${Date.now() - ts}ms.`)
-    client_instance = client
-    db_instance = client.db(db_name)
-    exec_callbacks(cb)
-  }).catch(err => {
-    is_preparing = false
-    console.log('Failed to connect. Error:', err)
+  connect_promise = new Promise((resolve, reject) => {
+    const ts = Date.now()
+    db.connect(url, { useNewUrlParser: true }).then(client => {
+      console.log(`Connect MongoDB cost ${Date.now() - ts}ms.`)
+      resolve(client.db(db_name))
+    }).catch(error => {
+      console.log('Failed to connect MongoDB. Error:', error)
+      reject(error)
+    })
+  })
+  return connect_promise
+}
+
+const DB_Table = table_name => {
+  const get = query => new Promise((resolve, reject) => connect().then(db_instance => {
+    const ts = Date.now()
+    db_instance.collection(table_name).findOne(query).then(entry => {
+      console.log(`Get operation on ${table_name} cost ${Date.now() - ts}ms.`)
+      resolve(entry)
+    }).catch(error => {
+      console.log(`Failed to get on ${table_name}. Error:`, error)
+      reject(error)
+    })
+  }).catch(error => {
+    reject(error)
+  }))
+
+  const list = query => new Promise((resolve, reject) => connect().then(db_instance => {
+    const ts = Date.now()
+    db_instance.collection(table_name).find(query).toArray().then(entries => {
+      console.log(`List operation on ${table_name} cost ${Date.now() - ts}ms.`)
+      resolve(entries)
+    }).catch(error => {
+      console.log(`Failed to list on ${table_name}. Error:`, error)
+      reject(error)
+    })
+  }).catch(error => {
+    reject(error)
+  }))
+
+  const insert = (obj, cb) => new Promise((resolve, reject) => connect().then(db_instance => {
+    const ts = Date.now()
+    db_instance.collection(table_name).insertOne(obj).then(({ result, ops: [{ _id }] }) => {
+      console.log(`Insert operation on ${table_name} cost ${Date.now() - ts}ms.`)
+      resolve({ result, _id })
+    }).catch(error => {
+      console.log(`Failed to insert on ${table_name}. Error:`, error)
+      reject(error)
+    })
+  }).catch(error => {
+    reject(error)
+  }))
+
+  const update = (filter, obj) => new Promise((resolve, reject) => connect().then(db_instance => {
+    const ts = Date.now()
+    db_instance.collection(table_name).updateMany(filter, {
+      $set: obj
+    }).then(({ result }) => {
+      console.log(`UpdateWith operation on ${table_name} cost ${Date.now() - ts}ms.`)
+      resolve(result)
+    }).catch(error => {
+      console.log(`Failed to update on ${table_name}. Error:`, error)
+      reject(error)
+    })
+  }).catch(error => {
+    reject(error)
+  }))
+
+	const exec = () => new Promise((resolve, reject) => connect().then(db_instance => {
+		const ts = Date.now()
+    const log_ts = () => console.log(`Free querying on ${table_name} cost ${Date.now() - ts}ms.`)
+    resolve({
+      table: db_instance.collection(table_name),
+      log_ts
+    })
+  }).catch(error => {
+    reject(error)
+  }))
+
+  return ({
+    get,
+    list,
+    insert,
+    update,
+    exec
   })
 }
 
-module.exports = collection_name => {
-
-  const insert = (obj, cb) => connect(() => {
-    const ts = Date.now()
-    db_instance.collection(collection_name).insertOne(obj).then(({ result, ops: [{ _id }] }) => {
-      cb && cb({ result, _id })
-      console.log(`Insert operation on ${collection_name} cost ${Date.now() - ts}ms.`)
-    }).catch(err => {
-      console.log('Failed to insert:', obj, 'Error:', err)
-    })
-  })
-
-  const update = (filter, obj, cb) => connect(() => {
-    const ts = Date.now()
-    db_instance.collection(collection_name).updateMany(filter, {
-      $set: obj
-    }).then(({ result }) => {
-      cb && cb(result)
-      console.log(`UpdateWith operation on ${collection_name} cost ${Date.now() - ts}ms.`)
-    }).catch(err => {
-      console.log('Failed to update:', obj, 'Error:', err)
-    })
-  })
-
-  const list = (filter, cb) => connect(() => {
-    const ts = Date.now()
-    db_instance.collection(collection_name).find(filter).toArray().then(arr => {
-      cb && cb(arr)
-      console.log(`List operation on ${collection_name} cost ${Date.now() - ts}ms.`)
-    }).catch(err => {
-      console.log('Failed to list. Error:', err)
-    })
-  })
-
-  const get = (query, cb) => connect(() => {
-    const ts = Date.now()
-    db_instance.collection(collection_name).findOne(query).then(entry => {
-      cb && cb(entry)
-      console.log(`Get operation on ${collection_name} cost ${Date.now() - ts}ms.`)
-    }).catch(err => {
-      console.log('Failed to get. Error:', err)
-    })
-  })
-
-	const exec = cb => connect(() => {
-		const ts = Date.now()
-    cb && cb(
-			db_instance.collection(collection_name),
-			() => console.log(`Free querying on ${collection_name} cost ${Date.now() - ts}ms.`)
-		)
-  })
-
-  return ({
-    insert, list, get, update, exec
-  })
+const db_name = 'task_server'
+module.exports = {
+  user: DB_Table('user'),
+  task: DB_Table('task')
 }
