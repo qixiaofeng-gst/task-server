c4b60914c038e142b39e42b05157f2d676f933a3
XiaofengQi <qixiaofeng@gsegment.com>
2019-11-05 10:27:39 +0800

    Formatted code with webstorm.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const app_port = 3100
 
 const ss = require('./libs/ss')
 
-const sessions = new ss(0) // permenant, so never use sessions.clear()
-const login_msg = { msg: 'login please' }
+const sessions = new ss(0) // permanent, so never use sessions.clear()
+const login_msg = {msg: 'login please'}
 app.use((req, res, next) => { // Middleware to take care of sessions.
-  const { query: { session }, path } = req
-	
+  const {query: {session}, path} = req
+
   console.log(`Accessing path: ${path} at ${(new Date()).toLocaleString()}.`)
-	
+
   if ('/user' === path) {
-		sessions.clear(session)
-		req.sessions = sessions
+    sessions.clear(session)
+    req.sessions = sessions
     next()
     return
   }
@@ -23,11 +23,11 @@ app.use((req, res, next) => { // Middleware to take care of sessions.
     return
   }
 
-	req.session = sessions.get(session)
+  req.session = sessions.get(session)
   next()
 })
 
-app.post('/', (req, res) => res.json({ message: 'Welcome to task manager.' }))
+app.post('/', (req, res) => res.json({message: 'Welcome to task manager.'}))
 app.use('/user', require('./routers/user'))
 app.use('/task', require('./routers/task'))
 
